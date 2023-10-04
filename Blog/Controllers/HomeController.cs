c6c785fcd61c26a0faab628c175ca051d45e7c63
Everson Rezende <rezende.everson@gmail.com>
2023-10-03 23:16:06 -0300

    Criando o banco e o controller de Category.

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [Route("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
