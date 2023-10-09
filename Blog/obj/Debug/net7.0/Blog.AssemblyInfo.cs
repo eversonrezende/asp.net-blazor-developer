029786a4b5c435e8e869f2e29668a7f8de26e8d5
Everson Rezende <rezende.everson@gmail.com>
2023-10-09 20:41:59 -0300

    config

--- Blog/obj/Debug/net7.0/Blog.AssemblyInfo.cs
+++ Blog/obj/Debug/net7.0/Blog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+545f809aedd3f21334498b6581d6e554967a1731")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef48d8220cd1ee00ad8501657e6e849faf661a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
