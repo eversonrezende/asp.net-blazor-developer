e6ffa5f235915e922b7bb789b442b06fec196016
erezendeosg <erezende@osg.com.br>
2023-11-03 08:51:37 -0300

    add page category, html, css e js.

--- MyRazorApp/Pages/Index.cshtml.cs
+++ MyRazorApp/Pages/Index.cshtml.cs
@@ -5,19 +5,9 @@ namespace MyRazorApp.Pages;
 
 public class IndexModel : PageModel
 {
-    public List<Category> Categories { get; set; } = new();
-
-    public async Task OnGet()
+    public void OnGet()
     {
-        await Task.Delay(5000);
-        for (var i = 0; i <= 100; i++)
-        {
-            Categories.Add(
-                new Category(i,
-                    $"Categoria {i}",
-                    i * 18.95M));
-        }
     }
+
 }
 
-public record Category(int Id, string Title, decimal Price);
