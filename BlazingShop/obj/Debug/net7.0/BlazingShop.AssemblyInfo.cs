4fdef93dbdabb09a4c7b58249ac16fcc87f9c079
Everson Alves de Rezende <erezende@osg.com.br>
2024-05-24 09:13:47 -0300

    create product form

--- BlazingShop/obj/Debug/net7.0/BlazingShop.AssemblyInfo.cs
+++ BlazingShop/obj/Debug/net7.0/BlazingShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9fe9be17213f59897ed24b45be0203b74844c5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb87cda04a963922fd22fac686fb0c258f08c0a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
